6c6f919b079dc41dec00a15a6b79cbeba3602cbc
NickO <nickotradovec589@gmail.com>
2021-03-03 14:04:41 -0700

    PE 742 Attempts

--- PE742_2.cs
+++ PE742_2.cs
@@ -7,9 +7,10 @@ using System.Linq;
 namespace ProjectEuler {
     public class PE742_2 : ISolve {
 
-        public const int sides = 96;     
+        public const int sides = 1000;     
         public Primes primes;       
         public List<Segment> segments = new List<Segment>();
+        double minArea;
         
         public void SetData() {
             primes = new Primes(sides);
@@ -21,24 +22,54 @@ namespace ProjectEuler {
                 }
             }
 
-            
+            minArea = 10000000000D;            
         }
 
         public void Solve() {
             
             // grab segments in order of ascending length until we have our necessary count
-            var used = new List<Segment>(sides/8);
-            used.Add(new Segment(1, 1));
-
-            segments.Sort(new SetmentSort_Length());
+            var minSegments = new List<Segment>((sides/4)-1);
+            
+            segments.Sort(new SegmentSort_Length());
             int i=0;
             while(i < (sides/8) - 1) {
-                used.Add(segments[i]);
+                minSegments.Add(segments[i]);
                 i++;
             }
+            minSegments.Add(new Segment(1, 1));
+            Reflect(ref minSegments);
 
-            // sort according to slope as that will be how they are connected.
-            used.Sort(new SetmentSort_Slope());
+            int testQuantity = 20; // this means highest 10 positions will be substituted with lowest 20 available values
+            var tests = ChooseFour(testQuantity*2);// ChooseThree(testQuantity*2);
+
+            List<Segment> testSegs;
+            int minSwapIndex = sides/8 - 1 - 4;
+            int[,] arc;
+            double area;
+
+            foreach(int[] test in tests) {
+                if ( test == null) {break;}
+                
+                testSegs = minSegments.ConvertAll(seg => new Segment(seg.points.Item1, seg.points.Item2));
+                for(int j=0; j<test.Length; j++) {
+                    testSegs[minSwapIndex + j] = segments[minSwapIndex+test[j]];
+                }
+
+                Reflect(ref testSegs);
+                testSegs.Sort(new SegmentSort_Slope());
+                arc = Arc(testSegs);
+                area = Area(arc);
+                if (area < minArea) {
+                    Console.WriteLine($"Min area: {area} found at vals: ({test[0]},{test[1]},{test[2]})");
+                    minArea = area;
+                }
+            }
+
+
+            Console.WriteLine(minArea);               
+        }
+
+        public int[,] Arc(List<Segment> segments) {
 
             // form useful points for the arc.
             int[,] arc = new int[(sides/4),2];
@@ -48,20 +79,31 @@ namespace ProjectEuler {
             do {
                 arc[j,0] = x;
                 arc[j,1] = y;
-                x += used[j].points.Item1;
-                y += used[j].points.Item2;
+                if (j >= segments.Count) {break;}
+                x += segments[j].points.Item1;
+                y += segments[j].points.Item2;
                 j++;
-            } while (j < used.Count);
-            arc[used.Count,0] = x;
-            arc[used.Count,1] = y;
+            } while (true);
+            return arc;
+        }
 
-            // reflecting along the diagonal,
-            for(int k=1; k<used.Count; k++) {
-                x += used[used.Count-k-1].points.Item2; // x and y should be reversed for hte reflection here.
-                y += used[used.Count-k-1].points.Item1;
-                arc[used.Count+k,0] = x;
-                arc[used.Count+k,1] = y;
+        public void Reflect(ref List<Segment> segments) {
+
+            int quantity = sides/8;
+            Segment reflect;
+            var blnAdd = (segments.Count <= quantity);
+
+            for(int k=1; k<quantity; k++) {
+                reflect = segments[quantity - 1 - k];
+                if (blnAdd) {
+                    segments.Add(new Segment(reflect.points.Item2, reflect.points.Item1));
+                } else {
+                    segments[quantity+k-1] = new Segment(reflect.points.Item2, reflect.points.Item1);
+                }
             }
+        }
+
+        public double Area(int[,] arc) {
 
             // now calculate area. This is specific to N divisible by 8.
             int arcMaxIdx = arc.GetLength(0)-1;
@@ -70,9 +112,7 @@ namespace ProjectEuler {
             for(int m=1; m<=arcMaxIdx; m++) {
                 area += (double)((double)(arc[m,0] - arc[m-1,0]) * ((double)maxY - (double)(arc[m,1] + arc[m-1,1])/2 ));
             }
-            area *= 4;
-            Console.WriteLine($"Last point at: ({arc[arcMaxIdx,0]},{arc[arcMaxIdx,1]})");
-            Console.WriteLine(area);                 
+            return area*4D;
         }
 
         public class Segment {
@@ -86,18 +126,48 @@ namespace ProjectEuler {
             }            
         }
 
-        public class SetmentSort_Length : IComparer<Segment> {
+        public class SegmentSort_Length : IComparer<Segment> {
             public int Compare(Segment a, Segment b) {
                 if (Math.Round(a.length, 12) > Math.Round(b.length, 12)) { return 1;}
                 if (Math.Round(a.length, 12) == Math.Round(b.length, 12)) { return 0;}
                 return -1;
             }
         }
-        public class SetmentSort_Slope : IComparer<Segment> {
+        public class SegmentSort_Slope : IComparer<Segment> {
             public int Compare(Segment a, Segment b) {
                 if (a.slope > b.slope) { return 1;}
                 return -1;
             }
+        }
+
+        public int[][] ChooseThree(int quantity) {
+            var combinations = new int[quantity*(quantity-1)*(quantity-2)][];
+            int idx = 0;
+            for(int i=0; i<quantity; i++) {
+                for(int j=i+1; j<quantity; j++) {
+                    for(int k=j+1; k<quantity; k++) {                   
+                        combinations[idx] = new int[]{i,j,k};
+                        idx++;
+                    }
+                }
+            }
+            return combinations;
+        }
+
+        public int[][] ChooseFour(int quantity) {
+            var combinations = new int[quantity*(quantity-1)*(quantity-2)*(quantity-3)][];
+            int idx = 0;
+            for(int i=0; i<quantity; i++) {
+                for(int j=i+1; j<quantity; j++) {
+                    for(int k=j+1; k<quantity; k++) {  
+                        for(int l=k+1; l<quantity; l++) {                      
+                            combinations[idx] = new int[]{i,j,k,l};
+                            idx++;
+                        }
+                    }
+                }
+            }
+            return combinations;
         }
     }
 }
